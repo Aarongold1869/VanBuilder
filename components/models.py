52460576de0f93bcf1f34e8e45bc1fee197a40cc
Aaron Goldstein <aarongold1869@gmail.com>
2021-07-03 14:27:10 -0500

    finished water/ plumbing tables

--- components/models.py
+++ components/models.py
@@ -95,20 +95,257 @@ class HeatInsulation(models.Model):
     r_value = models.CharField(max_length=30, null=True, blank=True)
 
 # Electrical
+class Storage(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    battery_type = models.CharField(max_length=30, null=True, blank=True)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    storage_capacity = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
+class Wiring(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    power_transfer = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    unit_measure = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class WireInsulation(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    unit_measure = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Inverter(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    model = models.CharField(max_length=30, null=True, blank=True)
+    type = models.CharField(max_length=30, null=True, blank=True)
+    wattage = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    surge = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    efficiency = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class BusBar(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Breaker(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Shore(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class SystemMonitor(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class TwelveVolt(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Lighting(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class ElectricalComponents(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
 # Solar
+class SolarPanel(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    wattage = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    power_per_cell = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    num_cells = models.IntegerField(null=True, blank=True)
+    efficiency = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    walkable = models.BooleanField(default=False)
+    description = models.TextField(null=True, blank=True)
+
+class ChargeController(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    volts = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    amps = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
 # Climate Control
+class Fan(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    power_usage = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Heater(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    power_usage = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class AC(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    power_usage = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
 # Water / Plumbing
+class FreshWaterTank(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    volume = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class GrayWaterTank(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    volume = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class WaterHeater(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    volume = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    power_usage = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Pump(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    pressure = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    power_usage = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Tubing(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    item = models.CharField(max_length=30, null=True, blank=True)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    unit_measure = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Accumulator(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Filter(models.Model): 
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class Faucet(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class ShowerHead(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+class WaterLevelMonitor(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.PROTECT,default=None)
+    brand = models.CharField(max_length=30, null=True, blank=True)
+    dimensions = models.CharField(max_length=30, null=True, blank=True)
+    cost_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    weight_per_unit = models.DecimalField(max_digits=15,decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
 # Flooring / Walls
 
+
 # Bathroom
+
 
 # Kitchen
 
+
 # Decorative Finishing
+
 
 # Hardware
